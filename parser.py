1a643e1994632532be0f016b4d8065089ee8a90b
kum7283-maker <kum7283@gmail.com>
2026-08-13 22:02:29 +0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,58 +1,53 @@
-import requests
 import random
 
 
-def get_wb_products():
+def get_products():
+
     products = [
         {
+            "market": "🟣 Wildberries",
+            "name": "Органайзер для хранения вещей",
+            "price": "899 ₽",
+            "old_price": "1990 ₽",
+            "discount": "55%",
+            "rating": "4.9",
+            "reviews": "8500",
+            "link": "https://www.wildberries.ru/"
+        },
+
+        {
+            "market": "🟡 Ozon",
             "name": "Беспроводные наушники",
             "price": "1290 ₽",
             "old_price": "2990 ₽",
-            "discount": "-57%",
+            "discount": "57%",
             "rating": "4.8",
-            "link": "https://www.wildberries.ru/"
+            "reviews": "12000",
+            "link": "https://www.ozon.ru/"
         },
+
         {
+            "market": "🟣 Wildberries",
             "name": "Умные часы",
             "price": "1990 ₽",
             "old_price": "3990 ₽",
-            "discount": "-50%",
+            "discount": "50%",
             "rating": "4.7",
+            "reviews": "5400",
             "link": "https://www.wildberries.ru/"
-        }
-    ]
+        },
 
-    return random.choice(products)
-
-
-
-def get_ozon_products():
-    products = [
         {
+            "market": "🟡 Ozon",
             "name": "Портативная колонка",
             "price": "1590 ₽",
             "old_price": "2990 ₽",
-            "discount": "-47%",
+            "discount": "47%",
             "rating": "4.9",
-            "link": "https://www.ozon.ru/"
-        },
-        {
-            "name": "Массажёр для спины",
-            "price": "2490 ₽",
-            "old_price": "4990 ₽",
-            "discount": "-50%",
-            "rating": "4.8",
+            "reviews": "9200",
             "link": "https://www.ozon.ru/"
         }
     ]
 
+
     return random.choice(products)
-
-
-
-def get_random_product():
-
-    if random.choice([True, False]):
-        return get_wb_products()
-
-    return get_ozon_products()
